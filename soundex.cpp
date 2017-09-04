03ca91e2c481a964a818fd23ccab169d1955472f
mike_su <mike_su@nuuo.com>
2017-09-04 11:19:18 +0800

    Add test case for words without repeat soundex

--- soundex.cpp
+++ soundex.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <iostream>
+#include <array>
+#include <algorithm>
 
 #ifndef ONLINE_JUDGE
 #include "test_case.h"
@@ -11,7 +13,15 @@ inline void run_test(int argc, char **argv) {}
 using namespace std;
 
 string to_soundex(const string& word) {
+    const static array<char, 26> letter_to_soundex = 
+    {' ', '1', '2', '3', ' ', '1', '2', ' ', ' ', '2', '2', '4', '5', '5', ' ', '1', '2', '6', '2', '3', ' ', '1', ' ', '2', ' ', '2'};
+    //A,   B,   C,   D,   E,   F,   G,   H,   I,   J,   K,   L,   M,   N,   O,   P,   Q,   R,   S,   T,   U,   V,   W,   X,   Y,   Z
+
     string soundex = "";
+    for_each(word.begin(), word.end(), [&soundex](char letter) {
+        char soundex_letter = letter_to_soundex.at(letter - 'A');
+        if(soundex_letter != ' ') soundex.push_back(soundex_letter);
+    });
     return soundex;
 }
 
--- test_case.cpp
+++ test_case.cpp
@@ -8,3 +8,7 @@ extern string to_soundex(const string&);
 TEST(ToSoundex, HandleEmptyLine) {
     ASSERT_EQ("", to_soundex(""));
 }
+
+TEST(ToSoundex, HandleLineWithoutRepeat) {
+    ASSERT_EQ("25", to_soundex("KHAWN"));
+}
